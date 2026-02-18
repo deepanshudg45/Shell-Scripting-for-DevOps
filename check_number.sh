a42c35d50a8d40a940edc4b57b63f4e05e3b10b7
Deepanshu Gahatori <gahatorideep45@gmail.com>
2026-02-18 14:48:59 +0000

    added scripts

---
+++ check_number.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+read -p "Enter Number: " num
+
+if [ "$num" -gt 0 ]
+then
+	echo "$num is positive"
+
+elif [ "$num" -lt 0 ]
+then
+	echo "$num is negative"
+
+else
+	echo "The number is zero"
+fi
---
+++ greet.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+read -p "Enter your name: " Name
+read -p "Enter your favourite tool: " Tool
+
+echo "Hello $Name, your favourite tool is $Tool"
